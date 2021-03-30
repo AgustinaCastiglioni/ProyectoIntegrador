b5bad7e45d370511611bceff0737c8d6c88e8fb0
AgustinaCastiglioni <51095882+AgustinaCastiglioni@users.noreply.github.com>
2021-03-30 16:16:30 -0300

    css

--- proyectoBase/src/components/Tarjetas.js
+++ proyectoBase/src/components/Tarjetas.js
@@ -4,13 +4,13 @@ function Tarjeta(props) {
     return (
         
         <div class="cuerpo">
-            <div class="uk-card uk-card-default uk-card-body">
+            <div class="uk-card uk-card-default uk-card-body card">
        
             <div class="uk-card uk-card-default">
             <div class="uk-card-media-top">
                 <img src={props.infoTarjetas.image} alt=""/>
                 </div>
-                <div class="uk-card-body">
+                <div class="uk-card-body info">
                 <h3 class="uk-card-title" style={{color: "blue"}}> {props.infoTarjetas.name}</h3>
                 <p>{props.infoTarjetas.status} - {props.infoTarjetas.species}</p>
                 <p>{props.infoTarjetas.origin.name}</p>
